0c8bff0d8f25f889bb4ecc067c9bd222e7994ce5
ailtontmarques <ailton.marques@gmail.com>
2022-07-12 18:46:10 -0300

    Adding unit tests

--- tests/unit/routes/personRoutes.test.js
+++ tests/unit/routes/personRoutes.test.js
@@ -1,14 +1,56 @@
-const request = require("supertest");
-const app = require('../../../app');
-const personRoutes = require('../../../routes/personRoutes');
+const request = require('supertest');
+const formurlencoded = require('form-urlencoded').default;
 
-describe("Test the root path", () => {
-  test("It should response the GET method", done => {
+const app = require('../../../app');
+const getData = (params = {}) => {
+  return {
+    cpf:  params.cpf || '01234567890',
+    name: params.name || 'John Doe'
+  }
+};
+
+const USER_OBJECT =  expect.objectContaining({
+  persons : {
+    cpf: expect.any(String),
+    name: expect.any(String)
+  }
+});
+
+describe('Test the root path', () => {
+  test('It should response the GET method', done => {
     request(app)
-      .get("/")
+      .get('/')
       .then(response => {
         expect(response.statusCode).toBe(200);
         done();
       });
   });
+
 });
+
+describe('Person Routes', () => {
+  test('Adding New Person', async () => {      
+      const response = await request(app).post('/person/').send(formurlencoded(getData()));
+      
+      expect(response.statusCode).toEqual(201);
+      expect(typeof response.body).toBe('object');
+      expect(response.text).toBe('Ok');
+  });
+
+  test('Calling response GET method person', async () => {      
+    const response = await request(app).get('/person/01234567890');
+
+    expect(response.statusCode).toEqual(200);
+    expect(typeof response.body).toBe('object');
+    expect(response.text).toBe("{\"persons\":[{\"cpf\":\"01234567890\",\"name\":\"John Doe\"}]}"); 
+  });
+
+  test('Deleting a Person', async () => {
+    const response = await request(app).delete('/person/01234567890');
+
+    expect(response.statusCode).toEqual(200);
+    expect(typeof response.body).toBe('object');
+    expect(response.text).toBe('Ok');
+  });
+});
+
